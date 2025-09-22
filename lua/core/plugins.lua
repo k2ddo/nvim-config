dcb1b653aa775d7efb096865f9e49edfba174262
Vlad <vladkiddo@gmail.com>
2025-09-22 17:03:40 +0300

    Added transparent background

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -60,5 +60,6 @@ require("lazy").setup({
 
   -- Themes
   {"folke/tokyonight.nvim"},
-  {"catppuccin/nvim", name = "catppuccin", priority = 1000}
+  {"catppuccin/nvim", name = "catppuccin", priority = 1000},
+  {"xiyaowong/transparent.nvim"},
 })
---
+++ lua/plugins/transparent.lua
@@ -0,0 +1,12 @@
+require("transparent").setup({
+  extra_groups = {
+    "NeoTreeNormal",
+    "NeoTreeNormalNC",
+    "NeoTreeFloatNormal",
+    "NeoTreeTabInactive",
+    "NeoTreeTabActive",
+    "NeoTreeTabSeparatorInactive",
+    "NeoTreeTabSeparatorActive",
+  },
+})
+
